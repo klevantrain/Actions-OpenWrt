aeba19ca0e7859bbe3aba2478a185009790fc0b6
DongQiYu <dqylyln@163.com>
2020-05-07 10:22:37 +0800

    Update diy.sh
    update diy

--- diy.sh
+++ diy.sh
@@ -5,20 +5,22 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #=================================================
-# Modify default IP
+#### Modify default IP
 sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
-git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
+
+#### add ext luci-app
 git clone https://github.com/kenzok8/openwrt-packages package/openwrt-packages
 git clone https://github.com/kenzok8/small package/small
-# add theme
-git clone https://github.com/openwrt-develop/luci-theme-atmaterial package/luci-theme-atmaterial
-git clone https://github.com/apollo-ng/luci-theme-darkmatter.git
-# remove duplicate app-ssr-plus
+git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
+
+#### add theme
+#git clone https://github.com/openwrt-develop/luci-theme-atmaterial package/luci-theme-atmaterial
+#git clone https://github.com/apollo-ng/luci-theme-darkmatter.git
+
+#### remove duplicate app-ssr-plus
 rm -rf package/openwrt-packages/luci-app-ssr-plus
-# modify luc display 
+
+#### modify luc display 
 sed -i 's/LuCI Master/LuCI master by dqylyln/g' /usr/lib/lua/luci/version.lua
 sed -i '/luciversion/d' /usr/lib/lua/luci/version.lua
 echo 'luciversion = "19.07"' >> /usr/lib/lua/luci/version.lua
-# reupdate feeds
-./scripts/feeds update -a
-./scripts/feeds install
